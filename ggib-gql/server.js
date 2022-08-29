b197cc310fe14daee592acf6fc4053b283c6a721
Zach Legein <zach.legein@hilton.com>
2022-08-29 16:44:59 -0500

    initial commit

---
+++ ggib-gql/server.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var cors = require('cors');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    user(id: Int!): UserPost
+  }
+  type User {
+    name: String
+    fullName: String
+    id: Int!
+    avatar: String
+    posts: [Post]
+  }  
+  type Post {
+    title: String
+    content: String
+  }  
+  type Friend {
+    name: String
+    numberOfPosts: Int
+  }
+  type UserPost {
+    me: User
+    friends: [Friend]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  user: ({ id }) => {
+    return {
+      me: {
+        name: "John",
+        fullName: "John Doe",
+        id,
+        avatar: "avatar-undefined.jpg",
+        posts: [
+          {
+            title: "Post 1",
+            content: "Content 1"
+          },
+          {
+            title: "Post 3",
+            content: "Content 3"
+          }
+        ],
+      },
+      friends: [
+        {
+          name: "Jane",
+          numberOfPosts: 2
+        },
+        {
+          name: "Jack",
+          numberOfPosts: 0
+        }
+      ]
+    }
+  }
+};
+
+var app = express();
+app.use(
+  cors({
+    origin: /hilton\.com/,
+    methods: ['GET', 'HEAD', 'OPTIONS'],
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
